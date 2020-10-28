75f9b5026eb4c2faf6c9139d6a85d2835e59a505
demetrios babalis <deeliciousplum@gmail.com>
2020-10-28 12:12:11 -0400

    Create CSharpWindowsFormNote-001.cs

---
+++ CSharpWindowsFormNote-001.cs
@@ -0,0 +1,19 @@
+// Exploring the Windows Form for designing a GUI/UI
+
+//  ToolBox Common Controls:
+//    checkBox -
+        checkBox1.Checked = false;    //for (un)checking a checkBox
+    
+//    textBox -
+      textBox1.Text = "";             // for clearing a textBox
+      
+
+// Method call examples
+
+  ClearChkTextBoxes();                // for calling the method
+  
+  private void ClearChkTextBoxes()
+  {
+    textBox1.Text = "";
+    checkBox1.Checked = false;
+  }
