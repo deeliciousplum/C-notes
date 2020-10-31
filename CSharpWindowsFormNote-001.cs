5d3a713ee07c90a5e74a02cf73daf976e1a805f1
demetrios babalis <deeliciousplum@gmail.com>
2020-10-31 11:15:09 -0400

    Update CSharpWindowsFormNote-001.cs

--- CSharpWindowsFormNote-001.cs
+++ CSharpWindowsFormNote-001.cs
@@ -1,20 +1,23 @@
 // Exploring the Windows Form for designing a GUI/UI
 
-//  ToolBox Common Controls:
-//    checkBox -
+// ToolBox Common Controls:
+// checkBox -
         checkBox1.Checked = false;    //for (un)checking a checkBox
     
-//    textBox -
+// textBox -
         textBox1.Text = "";             // for clearing a textBox
       
 
 // Method call examples
 
-  ClearChkTextBoxes();                // for calling the method
+        ClearChkTextBoxes();                // for calling the method
   
-  private void ClearChkTextBoxes()
-  {
-    textBox1.Text = "";
-    checkBox1.Checked = false;
-  }
+        private void ClearChkTextBoxes()
+        {
+                textBox1.Text = "";
+                checkBox1.Checked = false;
+        }
+
+// listBox thingies
+        listBox1.Items.Add("My text to output in a listBox")'
 
